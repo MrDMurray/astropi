49360001206d458098d40e838bf702f641981e04
Daniel Murray <daniel.murray@stjosephsrush.ie>
2023-10-16 19:55:10 +0100

    updated

---
+++ Speed of ISS/iss_speed/iss_speed_Part_4.py
@@ -0,0 +1,55 @@
+from exif import Image
+from datetime import datetime
+import cv2
+import math
+
+def get_time(image):
+    with open(image, 'rb') as image_file:
+        img = Image(image_file)
+        time_str = img.get("datetime_original")
+        time = datetime.strptime(time_str, '%Y:%m:%d %H:%M:%S')
+    return time
+
+def get_time_difference(image_1, image_2):
+    time_1 = get_time(image_1)
+    time_2 = get_time(image_2)
+    time_difference = time_2 - time_1
+    return time_difference.seconds
+
+def convert_to_cv(image_1, image_2):
+    image_1_cv = cv2.imread(image_1, 0)
+    image_2_cv = cv2.imread(image_2, 0)
+    return image_1_cv, image_2_cv
+
+def calculate_features(image_1, image_2, feature_number):
+    orb = cv2.ORB_create(nfeatures = feature_number)
+    keypoints_1, descriptors_1 = orb.detectAndCompute(image_1_cv, None)
+    keypoints_2, descriptors_2 = orb.detectAndCompute(image_2_cv, None)
+    return keypoints_1, keypoints_2, descriptors_1, descriptors_2
+
+def calculate_matches(descriptors_1, descriptors_2):
+    brute_force = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    matches = brute_force.match(descriptors_1, descriptors_2)
+    matches = sorted(matches, key=lambda x: x.distance)
+    return matches
+
+def display_matches(image_1_cv, keypoints_1, image_2_cv, keypoints_2, matches):
+    match_img = cv2.drawMatches(image_1_cv, keypoints_1, image_2_cv, keypoints_2, matches[:100], None)
+    resize = cv2.resize(match_img, (1600,600), interpolation = cv2.INTER_AREA)
+    cv2.imshow('matches', resize)
+    cv2.waitKey(0)
+    cv2.destroyWindow('matches')
+
+
+image_1 = 'photo_1754.jpg'
+image_2 = 'photo_1755.jpg'
+
+
+time_difference = get_time_difference(image_1, image_2) # Get time difference between images
+image_1_cv, image_2_cv = convert_to_cv(image_1, image_2) # Create OpenCV image objects
+keypoints_1, keypoints_2, descriptors_1, descriptors_2 = calculate_features(image_1_cv, image_2_cv, 1000) # Get keypoints and descriptors
+matches = calculate_matches(descriptors_1, descriptors_2) # Match descriptors
+display_matches(image_1_cv, keypoints_1, image_2_cv, keypoints_2, matches) # Display matches
+
+#print(matches)
+#print(get_time_difference('photo_1754.jpg', 'photo_1755.jpg'))
